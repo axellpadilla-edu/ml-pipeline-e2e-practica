765aef7674796fa9f0b904085eaf00526ed9f54f
Axell <68310020+axellpadilla@users.noreply.github.com>
2025-11-25 23:51:11 +0000

    ✨ feat: agregar cambios de código para mejorar la funcionalidad

--- m5-forecast/src/m5_forecast/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Paquete para descargar precios de acciones de tecnología usando yfinance."""
-
-from m5_forecast.descargar_datos import descargar_datos_acciones, main
-
-__all__ = ["descargar_datos_acciones", "main"]
